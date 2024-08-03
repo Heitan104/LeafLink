712a777f43baa1c781e9725e143c578c2f51f4af
SukhpreetB04 <sbath2004@gmail.com>
2024-08-03 17:23:55 -0400

    frontend first page

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="bg-green-900 text-white py-4 text-center">
+        <div>
+        <p>&copy; 2024 LeafLink. All rights reserved.</p>
+        <p>Contact us at info@leaflink.com</p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
