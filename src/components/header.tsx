b1f81fffa03a096d4c9950042798483014a97476
SukhpreetB04 <sbath2004@gmail.com>
2024-08-04 04:13:57 -0400

    header changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-const Header = ({title}) => {
+interface HeaderProps {
+  title: string;
+}
+
+const Header = ({title}: HeaderProps) => {
   return (
     <header className="bg-green-900 text-white p-4 flex justify-between items-center">
       <div className="text-xl">
